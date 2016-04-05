fd7b93860fe062d730d5a919bf0cfa5bfbeeb356
Prateek Chandan <prateekchandan5545@gmail.com>
2016-04-05 18:52:13 +0530

    eye blink rate working

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -6,14 +6,14 @@
 lab-0.obj: ../lab-0.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Compiler'
-	"/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me --include_path="/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include" --include_path="/home/prateek/Desktop/Education/Embedded Systems/TivaWare_C_Series-2.1.2.111" -g --gcc --define=ccs="ccs" --define=PART_TM4C123GH6PM --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="lab-0.pp" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	"/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me --include_path="/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include" --include_path="/home/prateek/Desktop/Education/Embedded Systems/TivaWare_C_Series-2.1.2.111" -g --gcc --define=ccs="ccs" --define=PART_TM4C123GH6PM --display_error_number --diag_warning=225 --diag_wrap=off --printf_support=full --preproc_with_compile --preproc_dependency="lab-0.pp" $(GEN_OPTS__FLAG) "$(shell echo $<)"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Compiler'
-	"/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me --include_path="/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include" --include_path="/home/prateek/Desktop/Education/Embedded Systems/TivaWare_C_Series-2.1.2.111" -g --gcc --define=ccs="ccs" --define=PART_TM4C123GH6PM --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="tm4c123gh6pm_startup_ccs.pp" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	"/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me --include_path="/home/prateek/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include" --include_path="/home/prateek/Desktop/Education/Embedded Systems/TivaWare_C_Series-2.1.2.111" -g --gcc --define=ccs="ccs" --define=PART_TM4C123GH6PM --display_error_number --diag_warning=225 --diag_wrap=off --printf_support=full --preproc_with_compile --preproc_dependency="tm4c123gh6pm_startup_ccs.pp" $(GEN_OPTS__FLAG) "$(shell echo $<)"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- lab-0.c
+++ lab-0.c
@@ -1,6 +1,15 @@
 /*
  * Code for Drowsiness Detection
+ * COnnections :
+ * Heart Rate Sensor:
+ * Blac Wire - GND
+ * White - VBUS
+ * Grey - PD3
  *
+ * TCRT500
+ * A0(red tape)  - PD2
+ * GND(yell) - GND
+ * VCC(red) - +3.3V
  */
 #include <stdio.h>
 #include <stdint.h>
@@ -18,7 +27,8 @@
 #define LOCK_F (*((volatile unsigned long *)0x40025520))
 #define CR_F   (*((volatile unsigned long *)0x40025524))
 
-#define defaultThresh 2000
+#define defaultThresh 2500
+#define numSamples 60 // NO of samples to take average from
 /*
 
  * Function Name: setup()
@@ -71,6 +81,15 @@ void switchPinConfig(void)
 	GPIODirModeSet(GPIO_PORTD_BASE,GPIO_PIN_2|GPIO_PIN_3,GPIO_DIR_MODE_IN); // Set Pin-4 of PORT F as Input. Modifiy this to use another switch
 	GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_3);
 	GPIOPadConfigSet(GPIO_PORTD_BASE,GPIO_PIN_2|GPIO_PIN_3,GPIO_STRENGTH_12MA,GPIO_PIN_TYPE_STD_WPU);
+
+	// Following two line removes the lock from SW2 interfaced on PORTF Pin0 -- leave this unchanged
+	LOCK_F=0x4C4F434BU;
+	CR_F=GPIO_PIN_0|GPIO_PIN_4;
+
+	// GPIO PORTF Pin 0 and Pin4
+	GPIODirModeSet(GPIO_PORTF_BASE,GPIO_PIN_4|GPIO_PIN_0,GPIO_DIR_MODE_IN); // Set Pin-4 of PORT F as Input. Modifiy this to use another switch
+	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0);
+	GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4|GPIO_PIN_0,GPIO_STRENGTH_12MA,GPIO_PIN_TYPE_STD_WPU);
 }
 
 /*
@@ -152,7 +171,7 @@ volatile int Signal;
 volatile int IBI = 600;
 volatile int Pulse = false;
 volatile int QS = false;
-volatile int rate[10];
+volatile int rate[numSamples];
 volatile unsigned long sampleCounter = 0;
 volatile unsigned long lastBeatTime = 0;
 volatile int P =defaultThresh;
@@ -170,7 +189,7 @@ void calcBPM(int sig){
 	sampleCounter += 2;
 	int N = sampleCounter - lastBeatTime;
 
-	if(Signal < thresh && N > (IBI/5)*3){
+	if(Signal < thresh /*&& N > (IBI/5)*3*/){
 		if (Signal < T){
 			T = Signal;
 		}
@@ -186,7 +205,7 @@ void calcBPM(int sig){
 			lastBeatTime = sampleCounter;
 			if(secondBeat){
 				secondBeat = false;
-				for(i=0; i<=9; i++){
+				for(i=0; i<=numSamples-1; i++){
 					rate[i] = IBI;
 				}
 			}
@@ -197,13 +216,13 @@ void calcBPM(int sig){
 				return;
 			}
 			long long runningTotal = 0;
-			for(i=0; i<=8; i++){
+			for(i=0; i<numSamples-1; i++){
 				rate[i] = rate[i+1];
 				runningTotal += rate[i];
 			}
-			rate[9] = IBI;
-			runningTotal += rate[9];
-			runningTotal /= 10;
+			rate[numSamples-1] = IBI;
+			runningTotal += rate[numSamples-1];
+			runningTotal /= numSamples;
 			BPM = 60000/runningTotal;
 			QS = true;
 		}
@@ -211,21 +230,113 @@ void calcBPM(int sig){
 	if (Signal < thresh && Pulse == true){
 		Pulse = false;
 		amp = P - T;
-		thresh = amp/2 + T;
+		thresh = (amp*3)/4 + T;
 		P = thresh;
 		T = thresh;
 	}
-	/*if (N > 2500){
+	if (N > 2500){
 	    thresh = defaultThresh;
 	    P = defaultThresh;
 	    T = defaultThresh;
 	    firstBeat = true;
 	    secondBeat = false;
 	    lastBeatTime = sampleCounter;
-	  }*/
+	  }
 
 }
+#define numBlinkSamples 10
+volatile int ready=false;
+volatile int blinkTime = -1;
+volatile int midval = 0;
+volatile int lowval = 0;
+volatile int highval = 0;
+volatile int lowcount = 0;
+volatile int highcount = 0;
+volatile int blinkDuration[numBlinkSamples];
+volatile int curClosedCount = 0;
+volatile int curClosedSum = 0;
+volatile int iterator = 0;
+volatile float averageBlinkDuration;
+volatile int curThreshold = 1000;
+volatile int avgThreshold = 200;
+volatile int alreadyOpen = false;
+int count = 0;
+int countLimit = 5;
 
+void calcEyeBlink(int sig){
+	// Caliberation
+	if(!ready){
+		if (GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00){
+			//pressed
+			if(lowcount>=1000)
+				return;
+			lowval = ((lowval * lowcount) + sig)/ (lowcount+1);
+			lowcount+=1;
+		}else if(lowcount>=1000){
+			highval = ((highval * highcount) + sig) / (highcount+1);
+			highcount+=1;
+			if(highcount >=1000){
+				ready = true;
+				midval = (highval + lowval)/2;
+			}
+		}
+	}else
+	{
+		if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00){
+			lowval = 0;
+			lowcount = 0;
+			highval = 0;
+			highcount = 0;
+			ready = false;
+			midval = 0;
+			blinkTime = -1;
+			curClosedCount = 0;
+			curClosedSum = 0;
+			iterator = 0;
+			count = 0;
+			return;
+		}
+
+		if(sig < midval){
+			GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,4);
+			if (alreadyOpen && count < countLimit){
+				count ++;
+				return;
+			}
+			//closed eye
+			curClosedCount += 1;
+			if (curClosedCount >= curThreshold){
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_3,2);
+			}
+			alreadyOpen = false;
+			count = 0;
+		}
+		else{
+			GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+			if (!alreadyOpen && count < countLimit){
+				count ++;
+				return;
+			}
+
+			if(alreadyOpen)
+				return;
+
+			printf("%f\n", averageBlinkDuration);
+			alreadyOpen = true;
+			iterator = (iterator + 1)%numBlinkSamples;
+			curClosedSum += curClosedCount - blinkDuration[iterator];
+			blinkDuration[iterator] = curClosedCount;
+			averageBlinkDuration = curClosedSum/(float)numBlinkSamples;
+			curClosedCount = 0;
+			if (averageBlinkDuration >= avgThreshold){
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_3,2);
+			} else {
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_3,8);
+			}
+			count = 0;
+		}
+	}
+}
 
 void Timer0IntHandler(void)
 {
@@ -243,4 +354,7 @@ void Timer0IntHandler(void)
 	ADCSequenceDataGet(ADC0_BASE, 2, hrValue);
 
 	calcBPM(hrValue[0]);
+	// BPM is in the variable BPM Now
+	calcEyeBlink(irValue[0]);
+	// blinkTime has the average blink time. -1 means no value
 }
